7b4cbfebbd95b3939860f744612395f345287265
Vaibhav Patel <vaibhav150189@yahoo.com>
2024-12-27 14:24:19 +0530

    fixed process undefined

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -12,7 +12,7 @@ const mount = (el) => {
 
 
 // If we are in dev mode and running in isolation, then call mount immediately
-if(process.NODE_ENV === "developmemt"){
+if(process.env.NODE_ENV === "development"){
 	const devRoot = document.querySelector("#_marketing-dev-root");
 
 	if(devRoot){
