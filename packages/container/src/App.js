d3678dfa65fdef600cb20d50c888010618409456
Vaibhav Patel <vaibhav150189@yahoo.com>
2024-12-30 11:02:50 +0530

    prevent css collisions

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 import MarketingApp from "./components/MarketingApp";
 import { BrowserRouter } from "react-router-dom";
-import Header from './components/Header';
+import Header from "./components/Header";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
