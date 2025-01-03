35f53016f7f926f506b94e335e9722c258c5aa89
Vaibhav Patel <vaibhav150189@yahoo.com>
2025-01-03 11:13:00 +0530

    added routing changes

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up app
-const mount = (el) => {
+const mount = (el, {onNavigate, defaultHistory}) => {
+	const history = defaultHistory || createMemoryHistory();
+
+	if(onNavigate){
+		history.listen(onNavigate);
+	}
+
 	ReactDOM.render(
-		<App />,
+		<App history={history} />,
 		el
 	);
+
+	return {
+		onParentNavigate({pathname: nextPathname}){
+			const {pathname} = history.location;
+
+			if(pathname !== nextPathname){
+				history.push(nextPathname);
+			}
+		}
+	};
 };
 
 
@@ -16,7 +33,7 @@ if(process.env.NODE_ENV === "development"){
 	const devRoot = document.querySelector("#_marketing-dev-root");
 
 	if(devRoot){
-		mount(devRoot);
+		mount(devRoot, { defaultHistory: createBrowserHistory() });
 	}
 }  
 
